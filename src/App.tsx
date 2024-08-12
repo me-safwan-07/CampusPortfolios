f1d31cce75dd299d30f37b78cef04ff29320f069
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-08-12 13:20:48 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-500 text-lg font-bold my-4">
+        Campuse Portfolios
+      </p>
+    </>
+  )
+}
+
+export default App
